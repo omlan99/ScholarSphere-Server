32c2cbab46fe5d6e83b0f52307914fd5bd6fe8f3
omlan99 <omlan10616@gmail.com>
2025-01-20 19:25:43 +0600

    server inital setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+app.use(express())
+app.use(express.json())
+
+app.get('/', (req,res) =>{
+    res.send('Data coming soon')
+})
+
+app.listen(port, () =>{
+    console.log('app is running at port :', port)
+})
