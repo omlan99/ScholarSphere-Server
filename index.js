904039e0d606e28289d0bccdefe077cbcc86b484
omlan99 <omlan10616@gmail.com>
2025-01-25 13:28:58 +0600

    adedd expected api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express()
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000
-const stripe = require('stripe')(process.env.stripe_key)
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.stripe_key);
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_admin}:${process.env.DB_pass}@cluster0.e6udf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,105 +18,144 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    const shcholarshipCollection = client.db('ScholarshipDB').collection('Scholarship')
-    const userCollection = client.db('ScholarshipDB').collection('users')
-    const reviewCollection = client.db('ScholarshipDB').collection('reviews')
+    const shcholarshipCollection = client
+      .db("ScholarshipDB")
+      .collection("Scholarship");
+    const userCollection = client.db("ScholarshipDB").collection("users");
+    const reviewCollection = client.db("ScholarshipDB").collection("reviews");
+    const applicationCollection  = client.db('ScholarshipDB').collection('applications')
 
-        // jwt related api
-        app.post("/jwt", async (req, res) => {
-          const user = req.body;
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.access_token, {
+        expiresIn: "1h",
+      });
+
+      res.send(token);
+    });
+
+    // all sholarship data api
+    app.get("/scholarship", async (req, res) => {
+      const query = {};
+      const search = req.query?.search;
+      if (search) {
+        query.$or = [
+          { university_name: { $regex: search, $options: "i" } }, // Match name
+          { subject_name: { $regex: search, $options: "i" } }, // Match city
+          { scholarship_category: { $regex: search, $options: "i" } }, // Match country
+        ];
+      }
+      const result = await shcholarshipCollection.find(query).toArray();
+      res.send(result);
+    });
     
-          const token = jwt.sign(user, process.env.access_token, {
-            expiresIn: "1h",
-          });
-    
-          res.send(token);
-        });
-    app.get('/scholarship', async(req, res)=>{
-        const query ={}
-        const search = req.query?.search
-        if (search) {
-          query.$or = [
-            
-            { university_name: { $regex: search, $options: "i" } }, // Match name
-            { subject_name: { $regex: search, $options: "i" } }, // Match city
-            { scholarship_category: { $regex: search, $options: "i" } }, // Match country
-          ]
-        }
-        const result = await shcholarshipCollection.find(query).toArray()
-        res.send(result)
-    })
-    app.get('/scholarship/:id', async (req, res) =>{
+
+    // post scholarship api
+    app.post("/scholarship", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await shcholarshipCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // id specifice scholarship get api
+    app.get("/scholarship/:id", async (req, res) => {
       const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await shcholarshipCollection.findOne(query);
+      res.send(result);
+    });
+  // scholarship delete api
+    app.delete('/scholarship/:id', async (req, res) =>{
+      const id = req.params.id
       const query = {_id : new ObjectId(id)}
-      const result = await shcholarshipCollection.findOne(query)
+      const result = await shcholarshipCollection.deleteOne(query)
       res.send(result)
     })
-    // user api 
-    app.get('/users', async (req, res) =>{
-      const email = req.query?.email
-      if(email){
-        const query = {email : email}
-        const result = await userCollection.findOne(query)
-        if(result){
-          return res.send(result)
+    // user get  api
+    app.get("/users", async (req, res) => {
+      const email = req.query?.email;
+      if (email) {
+        const query = { email: email };
+        const result = await userCollection.findOne(query);
+        if (result) {
+          return res.send(result);
         }
-            
       }
-      const result = await userCollection.find().toArray()
-       res.send(result)
-    })
-    app.post('/users', async(req, res) =>{
-      const user = req.body
-      const query = {email : user.email}
-      const listedUser = await userCollection.findOne(query)
-      if(listedUser){
-       return res.send({message : "User aleready existed"})
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    //  user post api 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const listedUser = await userCollection.findOne(query);
+      if (listedUser) {
+        return res.send({ message: "User aleready existed" });
       }
-      const result = await userCollection.insertOne(user)
-      res.send(result)
-    } )
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+        // user delete api
+        app.delete("/users/:id", async (req, res) => {
+          const userId = req.params.id;
+          const query = { _id: new ObjectId(userId) };
+          const result = await userCollection.deleteOne(query);
+          res.send(result);
+        });
     
     // review collection
-    app.get('/reviews', async (req, res) =>{
-      const result = await reviewCollection.find().toArray() 
-      res.send(result)
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/reviews" , async(req,res) => {
+      const reviewData = req.body
+      const result = await reviewCollection.insertOne(reviewData)
+    })
+
+    // appliation api
+    app.get('/applications', async (req, res) =>{
+      const email = req.query?.email
+      if(email){
+        query ={email : email}
+        const result = await applicationCollection.find(query).toArray
+        res.send(result)
+      }
+        const result = await applicationCollection.find().toArray() 
+        res.send(result)
     })
 
     // payment intent
-    app.post('/create-payment-intent', async(req,res)=>{
-      const {price} = req.body
-      const amount = parseInt(price * 100)
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
-        amount : amount,
-        currency : 'usd',
-        payment_method_types : ["card"]
-      })
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
       res.send({
-        clientSecret : paymentIntent.client_secret
-      })
-    })
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
-    // user delete api 
-    app.delete('/users/:id', async(req, res) =>{
-      const userId  = req.params.id
-      console.log(req.params.id)
-      const query = {_id : new ObjectId(userId) }
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
-    
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // ); 
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -124,10 +163,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req,res) =>{
-    res.send('Data coming soon')
-})
+app.get("/", (req, res) => {
+  res.send("Data coming soon");
+});
 
-app.listen(port, () =>{
-    console.log('app is running at port :', port)
-})
+app.listen(port, () => {
+  console.log("app is running at port :", port);
+});
